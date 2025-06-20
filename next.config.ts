2fa189c65f3de9884523b35ccebcf6e4ea1cced1
Jawad1998-ship-it <jawad.taj1998@gmail.com>
2025-06-20 17:35:36 +0600

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
+const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  distDir: "out",
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
